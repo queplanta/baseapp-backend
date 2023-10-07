314c3c86924a08e80ed5271768a2c0c0ce5c6261
Alisson Patricio <eu@alisson.net>
2023-10-07 08:45:41 -0300

    packages testproject cleanup

--- baseapp-notifications/testproject/settings.py
+++ baseapp-notifications/testproject/settings.py
@@ -28,35 +28,6 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
-# Constance
-CONSTANCE_BACKEND = "constance.backends.database.DatabaseBackend"
-CONSTANCE_CONFIG = OrderedDict(
-    [
-        (
-            "USER_PASSWORD_EXPIRATION_INTERVAL",
-            (
-                365 * 2,
-                "The time interval (in days) after which a user will need to reset their password.",
-            ),
-        ),
-    ]
-)
-
-# Rest Framework
-REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-    ),
-    "DEFAULT_PAGINATION_CLASS": "baseapp_core.rest_framework.pagination.DefaultPageNumberPagination",
-    "PAGE_SIZE": 30,
-    "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.NamespaceVersioning",
-    "ORDERING_PARAM": "order_by",
-    "SEARCH_PARAM": "q",
-    "TEST_REQUEST_DEFAULT_FORMAT": "json",
-}
-
 # Auth
 AUTH_USER_MODEL = "testapp.User"
 AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend"]
@@ -85,37 +56,6 @@ PHONENUMBER_DB_FORMAT = "E164"
 
 # BRANCH.IO
 BRANCHIO_KEY = env("BRANCHIO_KEY", "N/A")
-
-TEMPLATES = [
-    {
-        "BACKEND": "django_jinja.backend.Jinja2",
-        "DIRS": ["templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "match_extension": ".j2",
-            "constants": {"URL": URL, "FRONT_URL": FRONT_URL},
-        },
-    },
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ]
-        },
-    },
-]
-
-# Must be absolute URLs for use in emails.
-MEDIA_ROOT = str(BASE_DIR.parent / "media")
-MEDIA_URL = "{url}/media/".format(url=URL)
-STATIC_ROOT = str(BASE_DIR.parent / "static")
-STATIC_URL = "{url}/static/".format(url=URL)
 
 # JWT Authentication
 SIMPLE_JWT = {
